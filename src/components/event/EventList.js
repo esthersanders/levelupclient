b87d9569e658eb511576dac023024cef7774540d
estherviolin <esthernaomi.violin@gmail.com>
2020-11-04 14:29:13 -0600

    gamers can join and leave events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react"
 import { EventContext } from "./EventProvider.js"
 
+
 export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -10,33 +11,31 @@ export const EventList = (props) => {
 
     return (
         <article className="events">
-                  
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    props.history.push({ pathname: "/events/new" })
-                }}
-            >Create New Event</button>
             <header className="events__header">
                 <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        props.history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
             {
                 events.map(event => {
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
                         <div>{event.description}</div>
-                        <div>{event.date}</div>
                         <div>
-                            {/* {
-                                new Date({event.date}).toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                }) */}
-                              {event.time}
-                            
+                            {event.date} @ {event.time}
                         </div>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id)}
+                                >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                                >Join</button>
+                        }
                     </section>
                 })
             }
