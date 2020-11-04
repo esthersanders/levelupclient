9fa946003d272e7e42b4c6b9352df07bc055618b
estherviolin <esthernaomi.violin@gmail.com>
2020-11-04 10:52:42 -0600

    create game and create event working

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,20 +2,36 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameProvider } from "./game/GameProvider"
 import { GameList } from "./game/GameList"
+import { EventProvider } from "./event/EventProvider"
+import { EventList } from "./event/EventList"
+import { GameForm } from "./game/GameForm"
+import { EventForm } from "./event/EventForm"
 
-export const ApplicationViews = () => {
+
+export const ApplicationViews = (props) => {
     return <>
         <main style={{
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
-            Application views
+
 
             <GameProvider>
-                <Route exact path="/">
-                    <GameList />
+                <Route exact path="/games">
+                    <GameList {...props}/>
                 </Route>
+                <Route exact path="/games/new" render={props => <GameForm {...props} />} />
             </GameProvider>
+
+            <EventProvider>
+                <GameProvider>
+                    <Route exact path="/events">
+                        <EventList {...props}/>
+                    </Route>
+                    <Route exact path = "/events/new" render = {props => <EventForm {...props} />} />
+                </GameProvider>
+            </EventProvider>
+
         </main>
     </>
 }
--- src/components/game/GameProvider.js
+++ src/components/game/GameProvider.js
@@ -4,8 +4,10 @@ export const GameContext = React.createContext()
 
 export const GameProvider = (props) => {
     const [ games, setGames ] = useState([])
+    const [ gameTypes, setTypes ] = useState([])
 
     const getGames = () => {
+       
         return fetch("http://localhost:8000/games", {
             headers:{
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`
@@ -14,9 +16,31 @@ export const GameProvider = (props) => {
             .then(response => response.json())
             .then(setGames)
     }
+    const createGame = (game) => {
+        return fetch("http://localhost:8000/games", {
+            method: "POST",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(game)
+         })
+            .then(getGames)
+
+    }
+    
+    const getGameTypes = () => {
+        return fetch("http://localhost:8000/gametypes", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+         })
+            .then(res => res.json())
+            .then(setTypes)
+    }
 
     return (
-        <GameContext.Provider value={{ games, getGames }} >
+        <GameContext.Provider value={{ games, setGames, getGames, createGame, getGameTypes, gameTypes, setTypes }} >
             { props.children }
         </GameContext.Provider>
     )
